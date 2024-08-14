f79cc98b78b9f7410a21e25a54f49d00f1b86dfb
Sergey Gornih <seregaland@yandex.ru>
2024-08-14 14:55:49 +0300

    fix

---
+++ server/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const express = require('express')
+//const sequelize = require('./db')
+//const {Plan, Distributionw, Reportdistribw} = require('./models/models')
+//const { Op } = require('sequelize')
+const cors = require('cors')
+const fs = require('fs');
+const https = require('https')
+//const Route = require('./routes/route')
+//const errorHandler = require('./middleware/ErrorHandling')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+// app.use(bodyParser.json({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true}));
+
+
+const start = async () => {
+    try {
+        //await sequelize.authenticate()
+        //await sequelize.sync()
+        
+        httpsServer.listen(port, async() => {
+            console.log('HTTPS Server Admin-resender-bot running on port ' + port);
+               
+        }); 
+
+    } catch (error) {
+        console.log('Подключение к БД сломалось!', error)
+    }
+}
+
+start()
